2c51134fbbd6d5fe96a4539e117a754c37ce0757
sonali9130 <sonaligupta9130@gmail.com>
2021-12-01 23:23:22 +0530

    Update script.js

--- To Do/script.js
+++ To Do/script.js
@@ -1,1 +1,13 @@
-
+let input=document.querySelector("#input-1");
+input.addEventListener("keypress",function(e){
+    if(e.code=="Enter"){
+        let ul=document.querySelector("#ul-1");
+        let li=document.createElement("li");
+        li.innerText=e.currentTarget.value;
+        ul.append(li);
+        li.addEventListener("dblclick",function(e){
+            e.currentTarget.remove();
+        });
+        e.currentTarget.value="";
+    }
+});
